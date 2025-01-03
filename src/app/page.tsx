085983ce4745b1d7dcc22d9f006690a895466467
Gopala Agrawal <agrawalgopala@gmail.com>
2025-01-03 17:22:09 +0530

    feat: title for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <div className="flex flex-col justify-center">
+
+    <h1 className="text-lg font-bold text-center">Ask Away, Know Me!!!</h1>
+
+
+  </div>;
 }
