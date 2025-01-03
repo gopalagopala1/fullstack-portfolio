90577b4ca7958cbc22e1d68d66817144f8b11ee6
Gopala Agrawal <agrawalgopala@gmail.com>
2025-01-03 16:58:40 +0530

    chore: creating separate pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,41 @@
-import Link from 'next/link';
+"use client";
 
-const Navigation = () => (
+import {
+  ABOUT_ROUTE,
+  CONTACT_ROUTE,
+  HOME_ROUTE,
+  PROJECTS_ROUTE,
+  SKILLS_ROUTE,
+} from "@/contants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navigation = () => {
+  const pathname = usePathname();
+  const getClasses = (route: string) =>
+    pathname === route ? "text-yellow-400" : "";
+
+  return (
     <nav>
-        <ul className="flex justify-center">
-            <Link href='/' className="mx-2">Home</Link>
-            <Link href="/about" className="mx-2">About</Link>
-            <Link href="/skills" className="mx-2">Skills</Link>
-            <Link href="/projects" className="mx-2">Projects</Link>
-            <Link href="/contact" className="mx-2">Contact</Link>
-        </ul>
+      <ul className="flex justify-center gap-3 font-medium">
+        <Link href={HOME_ROUTE} className={getClasses(HOME_ROUTE)}>
+          Home
+        </Link>
+        <Link href={ABOUT_ROUTE} className={getClasses(ABOUT_ROUTE)}>
+          About
+        </Link>
+        <Link href={SKILLS_ROUTE} className={getClasses(SKILLS_ROUTE)}>
+          Skills
+        </Link>
+        <Link href={PROJECTS_ROUTE} className={getClasses(PROJECTS_ROUTE)}>
+          Projects
+        </Link>
+        <Link href={CONTACT_ROUTE} className={getClasses(CONTACT_ROUTE)}>
+          Contact
+        </Link>
+      </ul>
     </nav>
-)
+  );
+};
 
 export default Navigation;
---
+++ src/contants.ts
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = '/';
+export const ABOUT_ROUTE = '/about';
+export const CONTACT_ROUTE = '/contact';
+export const PROJECTS_ROUTE = '/projects';
+export const SKILLS_ROUTE = '/skills';
