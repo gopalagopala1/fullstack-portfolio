180402c39a5d6a667d77469ea153a31c9f2fe170
Gopala Agrawal <agrawalgopala@gmail.com>
2025-01-03 18:25:57 +0530

    fix: desktop about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-start gap-6 mt-2">
-      <div className="border-pattern">
+    <div className="flex flex-col md:flex-row justify-start gap-6 mt-2 items-center">
+      <div className="w-full">
         <Image
           src="/profile.jpg"
           alt="Picture of the author"
           width={500}
           height={500}
+          className="border-pattern"
         />
       </div>
 
@@ -18,15 +19,12 @@ const Home = () => {
         </span>
         <br />
         <br />
-
         I am a Full-Stack Developer with a strong background in developing
         scalable web applications and a strong startup mindset.
         <br />
         <br />
-        
         Dedicated to delivering high-performance, user-centric solutions while
         fostering collaborative team environments.
-
       </p>
     </div>
   );
